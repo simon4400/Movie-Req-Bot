6641c512b6a7cc6dd712e2dda35b5e14279cb7fe
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-04-15 23:27:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', '')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/3Qb.jpg')).split()
 
 
 # Admins & Users
