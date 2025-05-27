5307a6de80fa5942293428aa3143326479168053
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-05-28 00:38:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/MOVIE-INSIDER-05-27'))
+PICS = (environ.get('PICS', 'https://graph.org/MOVIE-INSIDER-05-27')).split()
 
 # Admins & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # For Multiple Id Use One Space Between Each.
