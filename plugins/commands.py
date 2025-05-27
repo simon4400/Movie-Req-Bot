8750b6e711c2bb2bf18b706aa66cb574c89bc385
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-05-28 01:33:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1063,14 +1063,14 @@ async def deletemultiplefiles(bot, message):
         parse_mode=enums.ParseMode.HTML
     )
 
-# @Client.on_message(filters.command("shortlink"))
-# async def shortlink(bot, message):
-#    userid = message.from_user.id if message.from_user else None
-#    if not userid:
-#        return await message.reply(f"You are anonymous admin. Turn off anonymous admin and try again this command")
-#    chat_type = message.chat.type
-#    if chat_type == enums.ChatType.PRIVATE:
-#        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\n#Sample:-\n/shortlink kpslink.in CAACAgUAAxkBAAEJ4GtkyPgEzpIUC_DSmirN6eFWp4KInAACsQoAAoHSSFYub2D15dGHfy8E\n\nThat's it!!! Enjoy Earning Money 💲\n\n[[[ Trusted Earning Site - https://kpslink.in]]]\n\nIf you have any Doubts, Feel Free to Ask me - @kingvj01\n\n(Puriyala na intha contact la message pannunga - @kngvj01)</b>")
+ @Client.on_message(filters.command("shortlink"))
+ async def shortlink(bot, message):
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Turn off anonymous admin and try again this command")
+    chat_type = message.chat.type
+    if chat_type == enums.ChatType.PRIVATE:
+        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\n#Sample:-\n/shortlink kpslink.in CAACAgUAAxkBAAEJ4GtkyPgEzpIUC_DSmirN6eFWp4KInAACsQoAAoHSSFYub2D15dGHfy8E\n\nThat's it!!! Enjoy Earning Money 💲\n\n[[[ Trusted Earning Site - https://kpslink.in]]]\n\nIf you have any Doubts, Feel Free to Ask me - @kingvj01\n\n(Puriyala na intha contact la message pannunga - @kngvj01)</b>")
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         grpid = message.chat.id
         title = message.chat.title
@@ -1332,7 +1332,7 @@ async def remove_premium_cmd_handler(client, message):
         return
     if len(message.command) == 2:
         user_id = int(message.command[1])  # Convert the user_id to integer
-      #  time = message.command[2]
+        time = message.command[2]
         time = "1s"
         seconds = await get_seconds(time)
         if seconds > 0:
