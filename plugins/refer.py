1885f52f099b14d61ee6f8887ba49e0e979115b9
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-05-29 12:23:08 +0530

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -1,20 +1,27 @@
-from pyrogram import Client, filters
+import os, string, logging, random, asyncio, time, datetime, re, sys, json, base64
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from os import environ
-
-BOT_USERNAME = environ.get("BOT_USERNAME")
-REFERAL_COUNT = environ.get("REFERAL_COUNT", 20)
+from script import SUBSCRIPTION_TXT
+from Script import script
+from database.users_chats_db import (
+    db,
+    delete_all_referal_users,
+    get_referal_users_count,
+    get_referal_all_users,
+    referal_add_user
+)
+from info import BOT_USERNAME, ADMINS, PREMIUM_AND_REFERAL_MODE, REFERAL_PREMEIUM_TIME, REFERAL_COUNT, LOG_CHANNEL, GRP_LNK, SUPPORT_CHAT
 
 @Client.on_message(filters.command("refer") & filters.private)
 async def refer_handler(client, message):
     user = message.from_user
     refer_link = f"https://t.me/{BOT_USERNAME}?start=VJ-{user.id}"
 
-    text = f"""👥 **Your Referral Link**
+    text = f"""👥 Your Referral Link
 
-🔗 `{refer_link}`
+🔗 {refer_link}
 
-📣 Share this with friends and get **Premium** after **{REFERAL_COUNT} referrals**!
+📣 Share this with friends and get Premium after {REFERAL_COUNT} referrals!
 """
 
     buttons = [[
@@ -25,4 +32,4 @@ async def refer_handler(client, message):
         text,
         reply_markup=InlineKeyboardMarkup(buttons),
         quote=True
-  )
+    )
