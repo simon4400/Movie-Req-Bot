5498ea3762e37066f9b13df9bb06d48f174e8c30
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-05-30 10:18:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -980,7 +980,7 @@ async def requests(bot, message):
                     reported_post = await bot.send_message(chat_id=admin, text=f"<b>𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n\n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
                     success = True
            else:
-                if len(content) < 3:
+               if len(content) < 3:
                     await message.reply_text(
     "<b>You must type about your request [Minimum 3 Characters]. Requests can't be empty.</b>\n\n"
     "Enter any text. Example formats:\n"
