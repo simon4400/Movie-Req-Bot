e514793f03f10058942c6f3a66b2700ec52582e5
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-05-30 11:06:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1029,6 +1029,13 @@ async def show_options_handler(bot, query):
     data = query.data
     user_id = data.split("#")[1]
 
+ if query.from_user.id not in
+ADMINS:
+        await query.answer("Only 
+admins can view these options!",
+show_alert=True)
+        return
+     
     buttons = [
         [
             InlineKeyboardButton("❌ Unavailable", callback_data=f"unavailable#{user_id}"),
