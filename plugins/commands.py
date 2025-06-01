f2a1422c27d60d6446ed632b9000735402ec86a7
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-06-01 12:00:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1489,9 +1489,9 @@ And get **1 Month Premium** after **{REFERAL_COUNT}** ** successful referrals!**
 """
 
     buttons = [[
-        InlineKeyboardButton("👥 Invited Your Friends ", url=share_url),
+        InlineKeyboardButton("👥 Invited Your Friends ", url=share_url)
     ],[
-      #  InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start')
+        InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start')
     ]]
 
     await message.reply(
