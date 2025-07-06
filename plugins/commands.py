66126521b82ff15d794b973f5d89b227c56cdfd5
simon4400 <mymovie008@gmail.com>
2025-07-07 01:13:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -583,7 +583,7 @@ async def start(client, message):
             ]]
             text = "<b>ʜᴇʏ {} 👋,\n\n🔐 **Daily Verification Required!** Please verify yourself by clicking the below button to access this file.</b>"
             if PREMIUM_AND_REFERAL_MODE == True:
-                text += "<b> Subscribe to Premium for ad-free exprience\n\n💶 Send /plan to buy subscription</b>"
+                text += "<b><blockquote>Subscribe to Premium for ad-free exprience\n\n💶 Send /plan to buy subscription<blockquote></b>"
             await message.reply_text(
                 text=text.format(message.from_user.mention),
                 protect_content=True,
