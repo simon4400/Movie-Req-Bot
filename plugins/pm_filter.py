5829c9679345025b9f0f45464f07e7fa7042498c
simon4400 <mymovie008@gmail.com>
2025-07-07 00:26:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1827,17 +1827,18 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "start":
         if PREMIUM_AND_REFERAL_MODE == True:
             buttons = [[
-                InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+              #  InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true'),
+                InlineKeyboardButton('⛩ Bot Update Channel', url="https://t.me/SIMON_Bots")
             ],[
-              #  InlineKeyboardButton('ᴇᴀʀɴ ᴍᴏɴᴇʏ', callback_data="shortlink_info"),
-                InlineKeyboardButton('ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ', url=GRP_LNK)
+                InlineKeyboardButton('🀄️ Info', callback_data="shortlink_info"),
+                InlineKeyboardButton('🍿 Movie Group', url=GRP_LNK)
             ],[
-              #  InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
-              #  InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
+              #  InlineKeyboardButton('💌 Help', callback_data='help'),
+              #  InlineKeyboardButton('🎟 About', callback_data='about')
             ],[
-                InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ ᴀɴᴅ ʀᴇғᴇʀʀᴀʟ', callback_data='subscription')
+                InlineKeyboardButton('📌 Refer & Get Primium', callback_data='subscription')
             ],[
-                InlineKeyboardButton('ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url=CHNL_LNK)
+              #  InlineKeyboardButton('ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url=CHNL_LNK)
             ]]
         else:
             buttons = [[
@@ -1849,7 +1850,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
              #  InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
              #   InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
             ],[
-                InlineKeyboardButton('ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url=CHNL_LNK)
+              #  InlineKeyboardButton('ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url=CHNL_LNK)
             ]]
         if CLONE_MODE == True:
             buttons.append([InlineKeyboardButton('ᴄʀᴇᴀᴛᴇ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', callback_data='clone')])
@@ -1935,7 +1936,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
           #   InlineKeyboardButton('ʏᴛ-ᴅʟ', callback_data='ytdl'), 
           #   InlineKeyboardButton('ꜱʜᴀʀᴇ ᴛᴇxᴛ', callback_data='share')
          ], [
-             InlineKeyboardButton('🃏 Aᴅᴍɪɴ', url="https://t.me/OwnerUpdate"),
+             InlineKeyboardButton('🃏 Aᴅᴍɪɴ', url="https://t.me/SIMON_Bots"),
              InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start')
          ], [
            #  InlineKeyboardButton('ꜱᴛɪᴄᴋᴇʀ-ɪᴅ', callback_data='sticker'),
@@ -1957,7 +1958,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton(' Support Group 💬', url=GRP_LNK),
-            InlineKeyboardButton('📢 Updates', url="https://t.me/MOVIE_INSIDER1")
+            InlineKeyboardButton('📢 Updates', url="https://t.me/SIMON_Bots")
         ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
            # InlineKeyboardButton('❌ Close ❌', callback_data='close_data')
@@ -2220,7 +2221,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "tele":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-            InlineKeyboardButton("📢 Updates", url="https://t.me/MOVIE_INSIDER1")
+            InlineKeyboardButton("📢 Updates", url="https://t.me/SIMON_Bots")
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
@@ -2261,7 +2262,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "share":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-            InlineKeyboardButton("📢 Updates", url="https://t.me/MOVIE_INSIDER1")
+            InlineKeyboardButton("📢 Updates", url="https://t.me/SIMON_Bots")
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
@@ -2277,7 +2278,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "song":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-            InlineKeyboardButton("📢 Updates", url="https://t.me/MOVIE_INSIDER1")
+            InlineKeyboardButton("📢 Updates", url="https://t.me/SIMON_Bots")
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
@@ -2318,7 +2319,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "sticker":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-            InlineKeyboardButton("📢 Updates", url="https://t.me/MOVIE_INSIDER1")
+            InlineKeyboardButton("📢 Updates", url="https://t.me/SIMON_Bots")
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
@@ -2334,7 +2335,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
   #  elif query.data == "tamil_info":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-            InlineKeyboardButton("📢 Updates", url="https://t.me/MOVIE_INSIDER1")
+            InlineKeyboardButton("📢 Updates", url="https://t.me/SIMON_Bots")
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
