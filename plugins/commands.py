523d623f4dee1fdd7bcde2b378e20a4e6d80f9e9
simon4400 <mymovie008@gmail.com>
2025-07-07 09:46:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -581,7 +581,9 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton("How to verify", url=VERIFY_TUTORIAL)
             ]]
-            text = "<b>🔐 *Daily Verification Required!*\nPlease verify yourself by clicking the below button to access this file.\n\n> Subscribe to /premium for ad-free experience</b>"
+            text = "<b>🔐 *Daily Verification Required!*\nPlease verify yourself by clicking the below button to access this file.</b>"
+            if PREMIUM_AND_REFERAL_MODE == True:
+                        text += "<b>\n\n> Subscribe to /premium for ad-free experience</b>"
                     await message.reply_text(
                         text=text.format(message.from_user.mention),
                         protect_content=True,
